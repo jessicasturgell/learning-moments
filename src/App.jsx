e0adb68bfadb1724b50f82e6d2a6bf3c0fc94c22
Jessica Sturgell <sturgell01@gmail.com>
2024-05-29 16:05:18 -0400

    added all posts list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import "./App.css" 
+import { useEffect, useState } from "react";
+import { getAllPosts } from "./services/postService.jsx";
 
 export const App = () => {
-  return <div>Hello World!</div>
-}
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    getAllPosts().then((postsArray) => {
+      setAllPosts(postsArray);
+      console.log("Posts set!");
+    });
+  }, []); // ONLY runs on initial render of component
+
+  return (
+    <div>
+      <h1 className="header">Posts</h1>
+      <article>
+        {allPosts.map((post) => {
+          return (
+            <section className="postsview" key={post.id}>
+              <h2>{post.title}</h2>
+              <div>{post.topic.name}</div>
+              <footer>Likes: {post.likes.length}</footer>
+            </section>
+          );
+        })}
+      </article>
+    </div>
+  );
+};
